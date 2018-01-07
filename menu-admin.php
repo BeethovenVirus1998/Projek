41f81bcda503ab243863b9c107d997041358bab5
Shafa Dhiyanti <34527559+Shafadhiyan@users.noreply.github.com>
2018-01-07 08:11:06 +0700

    shafa

---
+++ menu-admin.php
@@ -0,0 +1,50 @@
+                  <li class="mt">
+                      <a href="home.php?page=admin">
+                          <i class="fa fa-home"></i>
+                          <span>Home</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="" >
+                          <i class="fa fa-database"></i>
+                          <span>Data Induk</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="?page=data_mahasiswa">Data Mahasiswa</a></li>
+                          <li><a  href="?page=data_dosen">Data Dosen</a></li>
+                      </ul>
+                    
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="?page=data_dosen" >
+                          <i class="fa fa-cog"></i>
+                          <span>Setup</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="?page=setup_kelas">Kelas</a></li>
+                          <li><a  href="?page=setup_matkul">Mata Kuliah</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="?page=setup_kelas" >
+                          <i class="fa fa-pencil-square-o"></i>
+                          <span>Penjadwalan</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="?page=jadwal_ruangkelas">Ruang Kelas</a></li>
+                          <li><a  href="?page=data_pengampu">Pengampu</a></li>
+                      </ul>
+    
+                  </li>
+                  
+                   <li class="sub-menu">
+                      <a href="logout.php" >
+                          <i class="fa fa-sign-out"></i>
+                          <span>Logout</span>
+                      </a>
+    
+                  </li>
+                  
+
